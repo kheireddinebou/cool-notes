74df366ca35f331443c386bb35ff591f32ee1edd
Kheiereddin Boukhatem <105202227+khiereddinbou@users.noreply.github.com>
2023-02-22 16:59:04 +0100

    change cors policy url

--- api/index.ts
+++ api/index.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://notes-upg8.onrender.com",
     credentials: true, //access-control-allow-credentials:true
   })
 );
